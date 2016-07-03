31d02e7a7d507cd35c9c1a804a46211f4d1b0ddc
0xC000005 <flexie@foxmail.com>
2016-07-04 00:29:45 +0800

    modify

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -15,6 +15,18 @@ void main() {
     position += port_byte_in(0x3d5);
     int offset_from_vga = position * 2;
     char *vga = 0xb8000;
-    vga[offset_from_vga] = 'X'; 
+    vga[offset_from_vga] = 'T'; 
     vga[offset_from_vga+1] = 0x0f;
+    vga[offset_from_vga+2] = 'i'; 
+    vga[offset_from_vga+3] = 0x0f;
+    vga[offset_from_vga+4] = 'n'; 
+    vga[offset_from_vga+5] = 0x0f;
+    vga[offset_from_vga+6] = 'y'; 
+    vga[offset_from_vga+7] = 0x0f;
+    vga[offset_from_vga+8] = 'O'; 
+    vga[offset_from_vga+9] = 0x0f;
+    vga[offset_from_vga+10] = 's'; 
+    vga[offset_from_vga+11] = 0x0f;
+    vga[offset_from_vga+12] = '!'; 
+    vga[offset_from_vga+13] = 0x0f;
 }
