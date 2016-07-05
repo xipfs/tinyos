06bb34741daafc3d1b2d088ebb186f541cea58e2
0xC000005 <flexie@foxmail.com>
2016-07-05 23:53:54 +0800

    TinyOS v0.1

--- src/libc/string.c
+++ src/libc/string.c
@@ -24,7 +24,8 @@ void hex_to_ascii(int n, char str[]) {
     int i;
     for (i = 28; i > 0; i -= 4) {
         tmp = (n >> i) & 0xF;
-        if (tmp == 0 && zeros == 0) continue;
+        if (tmp == 0 && zeros == 0) 
+        	continue;
         zeros = 1;
         if (tmp > 0xA) append(str, tmp - 0xA + 'a');
         else append(str, tmp + '0');
