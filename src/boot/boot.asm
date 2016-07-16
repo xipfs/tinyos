ed580f8e3350acd2e587f2ca8dd09563e11b34a6
0xC000005 <flexie@foxmail.com>
2016-07-17 00:10:36 +0800

    修改注释

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,11 +1,10 @@
 ; **************************************************************************
 ; *                                                                        *
-; *  文件名: boot.asm                                                      * 
+; *  文件名: boot.asm                                                      *
 ; *  作者:   0xC000005                                                     *
 ; *  日期:   2016/06/29                                                    *
 ; *  Blog:   www.codeidea.cn                                               *
-; *  功能:   负责把主硬盘的MBR后X个扇区加载到0:0x1000,然后移交控制权       *
-; *                                                                        *
+; *  功能:   负责把主硬盘的MBR后X个扇区加载到0:0x1000,然后移交控制权           *
 ; *                                                                        *
 ; **************************************************************************
 
@@ -16,13 +15,13 @@ boot:
     mov bp, 0x9000
     mov sp, bp
 
-    mov bx, MSG_REAL_MODE 
-    call log16	               ; 记录日志
+    mov bx, MSG_REAL_MODE
+    call log16	                 ; 记录日志
     call load_kernel	           ; 加载内核
     call jump_to_pm	             ; 跳转至保护模式
     jmp $
 
-%include "boot/log.asm"	     ; 加载日志模块
+%include "boot/log.asm"	         ; 加载日志模块
 %include "boot/disk.asm"	       ; 加载硬盘模块
 %include "boot/pm.asm"	         ; 加载保护模式模块
 
@@ -41,7 +40,7 @@ load_kernel:
 BEGIN_PM:
     mov ebx, MSG_PROT_MODE
     call log32
-    call KERNEL_OFFSET 
+    call KERNEL_OFFSET
     jmp $
 
 
