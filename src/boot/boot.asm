8190e1d8625adb45b86336ce8fa22355c464c505
0xC000005 <flexie@foxmail.com>
2016-07-17 10:44:58 +0800

    格式化代码

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,12 +1,9 @@
-; **************************************************************************
-; *                                                                        *
-; *  文件名: boot.asm                                                      *
-; *  作者:   0xC000005                                                     *
-; *  日期:   2016/06/29                                                    *
-; *  Blog:   www.codeidea.cn                                               *
-; *  功能:   负责把主硬盘的MBR后X个扇区加载到0:0x1000,然后移交控制权       *
-; *                                                                        *
-; **************************************************************************
+;Filename : boot.asm
+;Author   : 0xC000005
+;Website  : www.codeidea.cn                                             
+;Date     : 2016/06/29                                         
+;                                                      
+;负责把主硬盘的MBR后X个扇区加载到0:0x1000,然后移交控制权                                                          
 
 org 0x7c00
 KERNEL_OFFSET equ 0x1000
@@ -16,14 +13,14 @@ boot:
     mov sp, bp
 
     mov bx, MSG_REAL_MODE
-    call log16	                 ; 记录日志
-    call load_kernel	           ; 加载内核
-    call jump_to_pm	             ; 跳转至保护模式
+    call log16                   ; 加载日志记录模块
+    call load_kernel             ; 加载内核
+    call jump_to_pm              ; 跳转到保护模式
     jmp $
 
-%include "boot/log.asm"          ; 加载日志模块
-%include "boot/disk.asm"         ; 加载硬盘模块
-%include "boot/pm.asm"**         ; 加载保护模式模块
+%include "boot/log.asm"    
+%include "boot/disk.asm"  
+%include "boot/pm.asm" 
 
 [bits 16]
 load_kernel:
@@ -33,7 +30,7 @@ load_kernel:
     mov bx, KERNEL_OFFSET
     mov dh, 16
     mov dl, [BOOT_DRIVE]
-    call disk_load               ; 加载硬盘数据
+    call disk_load
     ret
 
 [bits 32]
